41e180f2c347009094e61b3801afc1f26c20d387
mmathews <mmathews@udel.edu>
2023-05-04 16:50:02 -0400

    changed formatting for learn more, need to fix colors

--- src/Pages/LearnMorePage.tsx
+++ src/Pages/LearnMorePage.tsx
@@ -4,27 +4,120 @@ import classes from "./LearnMorePage.module.css";
 export const LearnMorePage = (): JSX.Element => {
     return (
         <section className={classes.section_learn_more}>
-            <div className={classes.learn_more}>
-                <div className={classes.learn_more_text}>
-                    <h1 className="heading-primary">What is ProjectX?</h1>
-                    <p className={classes.learn_more_description}>
-                        ProjectX is an easy way to discover new Movies and Shows
-                        to watch. You can find top Movies and Shows by ratings
-                        or search through genres. Add any Movie or Show to a
-                        favorites list or a to watch list. Add ratings to your
-                        favorite Movies and Shows and check out your friends
-                        favorites too!
-                    </p>
-                </div>
-                <div className={classes.creators}>
-                    <div className={classes.creator_text}>
-                        <h1 className="heading-secondary">
-                            Creators of ProjectX
-                        </h1>
-                        <p className={classes.creators_names}>
-                            Abdulrahman Abdulhamid, Talha Mahmood, Joseph
-                            Hooper, Ian Steele, and Mercedes Mathews
-                        </p>
+            <div className={classes.lm_text}>
+                <h1 className={classes.lm_title}>What is ProjectX?</h1>
+                <p className={classes.lm_description}>
+                    ProjectX is an easy way to discover new movies and shows to
+                    watch! You can find top movies and shows by ratings or
+                    search through genres. Add any movie or show to a favorites
+                    list or a to-watch list and add ratings to your favorites.
+                    Check out what your friends are watching by looking at their
+                    favorites! Can&apos;t find a movie or show you like? Send in
+                    a request to have that media added!
+                </p>
+            </div>
+            <div className={classes.creators_text}>
+                <h2 className={classes.creators_title}>Creators of ProjectX</h2>
+                <div className={classes.row}>
+                    <div className={classes.column}>
+                        <div className={classes.card}>
+                            <img
+                                className={classes.creator_image}
+                                src={require("../imgs/creators/dog2.png")}
+                                alt="Abdulrahman Abdulhamid"
+                            />
+                            <div className={classes.container}>
+                                <h2>Abdulrahman Abdulhamid</h2>
+                                <p className={classes.creator_position}>
+                                    CEO & Founder
+                                </p>
+                                <p>
+                                    Some text that describes me lorem ipsum
+                                    ipsum lorem.
+                                </p>
+                                <p>bood@udel.edu</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className={classes.column}>
+                        <div className={classes.card}>
+                            <img
+                                className={classes.creator_image}
+                                src={require("../imgs/creators/dog2.png")}
+                                alt="Talha Mahmood"
+                            />
+                            <div className={classes.container}>
+                                <h2>Talha Mahmood</h2>
+                                <p className={classes.creator_position}>
+                                    CEO & Founder
+                                </p>
+                                <p>
+                                    Some text that describes me lorem ipsum
+                                    ipsum lorem.
+                                </p>
+                                <p>talha@udel.edu</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className={classes.column}>
+                        <div className={classes.card}>
+                            <img
+                                className={classes.creator_image}
+                                src={require("../imgs/creators/dog2.png")}
+                                alt="Talha Mahmood"
+                            />
+                            <div className={classes.container}>
+                                <h2>Talha Mahmood</h2>
+                                <p className={classes.creator_position}>
+                                    CEO & Founder
+                                </p>
+                                <p>
+                                    Some text that describes me lorem ipsum
+                                    ipsum lorem.
+                                </p>
+                                <p>talha@udel.edu</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className={classes.column}>
+                        <div className={classes.card}>
+                            <img
+                                className={classes.creator_image}
+                                src={require("../imgs/creators/dog2.png")}
+                                alt="Talha Mahmood"
+                            />
+                            <div className={classes.container}>
+                                <h2>Talha Mahmood</h2>
+                                <p className={classes.creator_position}>
+                                    CEO & Founder
+                                </p>
+                                <p>
+                                    Some text that describes me lorem ipsum
+                                    ipsum lorem.
+                                </p>
+                                <p>talha@udel.edu</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className={classes.column}>
+                        <div className={classes.card}>
+                            <img
+                                className={classes.creator_image}
+                                src={require("../imgs/creators/dog2.png")}
+                                alt="Talha Mahmood"
+                            />
+                            <div className={classes.container}>
+                                <h2>Talha Mahmood</h2>
+                                <p className={classes.creator_position}>
+                                    CEO & Founder
+                                </p>
+                                <p>
+                                    Some text that describes me lorem ipsum
+                                    ipsum lorem.
+                                </p>
+                                <p>talha@udel.edu</p>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
