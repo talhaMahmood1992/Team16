755425415019ef3d9e032526d775fd5dc04eb379
talha <talhamahmood2003@gmail.com>
2023-05-09 17:10:40 -0400

    Removed redudant code from Favorites, can only changle role from the main page

--- src/Pages/Favorites.tsx
+++ src/Pages/Favorites.tsx
@@ -1,24 +1,11 @@
 /* eslint-disable no-extra-parens */
-import React, { useState } from "react";
+import React from "react";
 // import RenderMedia from "../Components/RenderMedia";
-import { getUserByUsername } from "../UserData";
-import { Media, UserInterface } from "../Interfaces";
 import { DeleteMedia } from "../Components/RemoveMedia";
 interface FavoriteMediaProps {
     userName: string;
 }
 export const FavoritesPage = (props: FavoriteMediaProps): JSX.Element => {
-    const currentUser = getUserByUsername(props.userName);
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const [watched, setDleteMedia] = useState<Media[]>(
-        getWatchedList(currentUser)
-    );
-
-    function getWatchedList(user: UserInterface): Media[] {
-        const watchedList: Media[] = user.watched.map((item) => item.media);
-        return watchedList;
-    }
-
     return (
         <>
             <section className="page">
