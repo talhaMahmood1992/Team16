0973b17f575ff03c9127ca4b7a0d73a943ca55c8
Abdulrahman Abdulhamid <abdulrahman.t.abdulhamid@gmail.com>
2023-05-17 16:57:27 -0400

    Reflected changes in central list in super and admin list

--- src/Components/Forms/EditMediaForm/EditMediaForm.tsx
+++ src/Components/Forms/EditMediaForm/EditMediaForm.tsx
@@ -38,12 +38,24 @@ export const EditMediaForm = ({
     const onSubmit = async (data: EditMediaSubmitForm) => {
         try {
             await updateMedia(mediaItem._id, data);
+            let updatedMedia = editList.find(
+                (media: MediaInterface) => media._id === mediaItem._id
+            );
+            updatedMedia = {
+                ...data,
+                title: mediaItem.title,
+                image: slugify(mediaItem.title) + ".jpg",
+                _id: updatedMedia._id
+            };
+
             let updatedEditList = editList.filter(
                 (media: MediaInterface) => media._id !== mediaItem._id
             );
+            updatedEditList = [...updatedEditList, updatedMedia];
+
             updatedEditList = removeMediaId(updatedEditList);
             updatedEditList = removeImageFromMedia(updatedEditList);
-            console.log(updatedEditList);
+
             await updateUser("645e8ce9a3aae9249f9fdf2f", {
                 watched: updatedEditList,
                 toWatch: []
@@ -52,6 +64,7 @@ export const EditMediaForm = ({
                 watched: updatedEditList,
                 toWatch: []
             });
+            console.log("Updated Media in central, super, admin");
         } catch (error) {
             console.log(error);
         }
