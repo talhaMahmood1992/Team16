94367b4c0c788d9f7e3c55c9cf3d70a92dcb73f4
J-Hooper <joeyhoop@udel.edu>
2023-05-17 12:43:11 -0400

    Removing a media now removes it from every favorites list

--- src/Components/AddMediaForm/EditMediaForm.tsx
+++ src/Components/AddMediaForm/EditMediaForm.tsx
@@ -8,6 +8,7 @@ import axios from "axios";
 import { Media, Role, mediaType } from "../../Interfaces";
 import { useNavigate } from "react-router-dom";
 import { Form, Button } from "react-bootstrap";
+import { UserData } from "../../UserData";
 /* eslint no-extra-parens: "off" */
 
 interface EditMediaFormProps {
@@ -91,13 +92,24 @@ export const EditMediaForm = (props: EditMediaFormProps): JSX.Element => {
 
         const removeTitle = () => {
             const editableList = [...props.superList];
-            const index = editableList.findIndex(
+            const editIndex = editableList.findIndex(
                 (title) => title === props.media.title
             );
-            if (index >= 0) {
-                editableList.splice(index, 1);
+            if (editIndex >= 0) {
+                editableList.splice(editIndex, 1);
             }
-
+            UserData.forEach(function (user) {
+                let favIndex = user.watched.findIndex(
+                    (userMedia) => userMedia.media.title === props.media.title
+                );
+                while (favIndex >= 0) {
+                    user.watched.splice(favIndex, 1);
+                    favIndex = user.watched.findIndex(
+                        (userMedia) =>
+                            userMedia.media.title === props.media.title
+                    );
+                }
+            });
             return editableList;
         };
 
